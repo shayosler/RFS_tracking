268a598bb6f55a88921d7b3d5448cef023a305f7
Shay <iwishiwuzskiing@gmail.com>
2023-02-27 21:48:40 -0500

    updated doc for in_fo

--- GMPHD/in_fov.m
+++ GMPHD/in_fov.m
@@ -1,14 +1,14 @@
 function [in] = in_fov(x, objs, end_range, l_lim, r_lim)
-%[in] = in_fov(x, objs, range, l_lim, r_lim) Check if objects are in
+%[in] = in_fov(x, objs, range, l_lim, r_lim) Check if points are in
 %a sonar's field of view
 % Inputs:
 %   x       vehicle position 3x1, [n; e; psi]
-%   objs    Objects to check 2xM [n; e]
+%   objs    Points to check 2xM [n; e]
 %   end_range   Current sonar end range
 %   l_lim   Left limit of the sonar field of view relative to current
-%           vehicle heading, degrees
+%           vehicle heading, degrees (-180, 180]
 %   r_lim   Right limit of the sonar field of view relative to current
-%           vehicle heading, degrees
+%           vehicle heading, degrees (-180, 180]
 % Outputs:
 %   in      1xM logical array that is true for each object in the FOV
 
@@ -26,7 +26,6 @@ r = sqrt(sum(offset.^2, 1));
 b = atan2d(offset(2, :), offset(1, :)) - x(3);
 
 % Determine which targets are within the field of view
-% and which ones are actually detected
 in = r < end_range & b > l_lim & b < r_lim;
 
 end
