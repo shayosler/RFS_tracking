cad9babab7ed8a7b57242672eabd70559f65806d
Shay <iwishiwuzskiing@gmail.com>
2023-06-05 01:02:17 -0400

    adding plotting functions

---
+++ +RFS/+CPHD/lcphd_plots.m
@@ -0,0 +1,12 @@
+function [h] = lcphd_plots(t, targets, trackers, lambda, lambda_true)
+%lcphd_plots Plots specific to the lambda-CPHD filter
+
+% plot true number of targets, estimated number of targets, and estimated
+% number of clutter generators
+
+% Plot true vs estimated clutter rate (lambda)
+
+% Plot cardinality distribution
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
---
+++ +RFS/rfs_tracker_plots.m
@@ -0,0 +1,16 @@
+function h = rfs_tracker_plots(t, v, trackers, measurements, targets)
+% rfs_tracker_plots Generate plots common to all trackers
+
+% Plot current intensity
+
+% Plot target locations
+
+% Plot tracker locations
+
+% Plot measurements
+
+% Plot true vs estimated number of targets
+
+
+
+end
