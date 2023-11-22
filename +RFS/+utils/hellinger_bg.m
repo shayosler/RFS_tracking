4073405aa4c0dbfeacf457345cea02f52d6226fb
Shay <iwishiwuzskiing@gmail.com>
2023-11-22 05:14:27 -0500

    pruning/merging

---
+++ +RFS/+utils/hellinger_bg.m
@@ -0,0 +1,21 @@
+function d = hellinger_bg(m1, P1, s1, t1, m2, P2, s2, t2)
+%HELLINGER Hellinger distance between beta gaussian distributions
+% d = HELLINGER(m1, P1, s1, t1, m2, P2, s2, t2) calculates the Hellinger 
+% distance between two beta-gaussian distributions defined by parameters
+% m, P, s, and t
+% 
+% Inputs:
+%
+% Outputs:
+
+% TODO: make this accept a vector for each of m, P, s, t
+% Beta component of distance
+b = beta(mean([s1, s2]), mean([t1, t2])) / sqrt(beta(s1, t1) * beta(s2, t2));
+
+% gaussian component of distance
+g = sqrt(mvnpdf(0, m1 - m2, P1 + P2)) / det(8 * pi * (inv(P1) + inv(P2)))^.25;
+
+% Compute distance
+d = sqrt(1 - b * g);
+
+end
--- +RFS/+utils/prune_bgmrfs.m
+++ +RFS/+utils/prune_bgmrfs.m
@@ -1,11 +1,11 @@
-function [v_out] = prune_gmphd(v, T, U, Jmax)
+function [v_out] = prune_bgmrfs(v, T, U, Jmax)
 %[v_out] = prune_gmphd(v, T, U, Jmax) Prune the gaussian mixture components
 % representing a random finite set. Pruning is done by removing any 
 % components with weight less than T, then merging any components that are
 % closer together than U, and finally taking the Jmax highest weighted
 % components if there are still more than Jmax
 % Inputs
-%   v       Gaussian components representing the RFS
+%   v       Beta-Gaussian components representing the RFS
 %   T       Truncation threshold
 %   U       Merging threshold
 %   Jmax    Maximum number of components to have after pruning
@@ -35,11 +35,11 @@ l = 0;
 dim = size(v.m, 1);
 while any(I)
     l = l + 1;
-    L = [];
-    % Find all components that are close enough to that
-    % Largest component to merge
+    % Find all components that are "close" enough to the
+    % highest weighted remaining component to merge
     [~, i_max] = max(v.w(I));
     j = idxs(i_max);
+    L = []; % Component indices to merge
     dist = zeros(sum(I), 1);
     for i = find(I)'
         dist(i) = (v.m(:, i) - v.m(:, j))' * ( v.P(:, :, i) \ (v.m(:, i) - v.m(:, j)));
@@ -59,12 +59,20 @@ while any(I)
     w = [w wl];
     m = [m ml];
     P = cat(3, P, Pl);
+    sigsq_beta = v.s(L) .* v.t(L) ./((v.s(L) + v.t(L)^2 .* (v.s(L) + v.t(L) + 1)))    ;
+    mu_beta = v.s(L) ./ (v.s(L) + v.t(L));
+    sigsq_beta_merged = 1 / wl * (v.w(L) * sigsq_beta');
+    mu_beta_merged = 1 / wl * (v.w(L) * mu_beta');
+    s = 
+    t = 
 
     % Remove merged components
     I(L) = false;
     idxs = find(I)';
 end
 
+% If there are still more than Jmax components, remove the lowest weighted
+% ones
 if numel(w) > Jmax
     [~, imax] = maxk(w, Jmax);
     w = w(imax);
@@ -73,11 +81,9 @@ if numel(w) > Jmax
 end
 
 % Renormalize weights so that the total weight stays constant
-% TODO: this wasn't done in the paper, but it seems like a step that should
-% happen
 sum_unpruned = sum(v.w);
 sum_pruned = sum(w);
 w = (sum_unpruned / sum_pruned) .* w;
 
-v_out = RFS.utils.GMRFS(m, P, w);
+v_out = RFS.utils.BGMRFS(w, m, P, s, t);
 end
